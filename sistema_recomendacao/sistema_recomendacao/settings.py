3223a1464e7517ce8d634578f9fa456e2a72725f
Tamara Lécia <tamaralecia976@gmail.com>
2025-10-20 09:10:32 -0300

    Tentativa de Deploy com o SQLite na platforma render

--- sistema_recomendacao/sistema_recomendacao/settings.py
+++ sistema_recomendacao/sistema_recomendacao/settings.py
@@ -141,7 +141,8 @@ WSGI_APPLICATION = 'sistema_recomendacao.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # eu acrescentei o os.path.join no name
+        'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
     }
 }
 
